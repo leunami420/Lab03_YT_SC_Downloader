6bda7d47549ffb8874331305f09b04725b5c442a
leunami420 <marconich311@gmail.com>
2023-05-02 15:17:22 +0200

    v1.2.0

--- project/gui.py
+++ project/gui.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 import os
 import subprocess
+from tkinter import StringVar
+
 import requests
 from io import BytesIO
 from PIL import Image
@@ -29,6 +31,7 @@ class App(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
+        self.title("YTDownloader")
         self.selectedFormat = "Video"
         self.geometry("720x600")
         self.iconbitmap("image.ico")
@@ -39,12 +42,31 @@ class App(customtkinter.CTk):
         self.format_id = ""  # format id of the video/audio
         self.ext = ""  # file extension of the video/audio
         self.filesize = "" # size of the video/audio file in bytes
-        self.title("Downloader")
+        self.openFolder = True
+
 
         self.label = customtkinter.CTkLabel(master=self, text="YouTube and SoundCloud Downloader")
         self.label.pack()
 
-        self.url_entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter a valid URL", width=500)
+        def updateMediaInfo(url):
+            ydl_opts = {}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                video_info = ydl.extract_info(url, download=False)
+                self.thumbnail_url = video_info.get("thumbnail")
+                self.videotitle = video_info.get('title')
+                self.duration = video_info.get("duration")
+                self.author = video_info.get("uploader")
+                self.format_id = video_info.get("format_id")  # format id of the video/audio
+                self.ext = video_info.get("ext")  # file extension of the video/audio
+                self.filesize = video_info.get("filesize")  # size of the video/audio file in bytes
+
+        def callback(str_var):
+            updateMediaInfo(str_var.get())
+            updateThumbnail(self.thumbnail_url)
+
+        url_var = StringVar()
+        self.url_entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter a valid URL", width=500, textvariable=url_var)
+        url_var.trace("w", lambda name, index, mode, url_var=url_var: callback(url_var))
         self.url_entry.pack(padx=10, pady=10)
 
         def updateProgress(d):
@@ -55,7 +77,8 @@ class App(customtkinter.CTk):
                     self.label.configure(text=f"{percent}")
                 elif status == 'finished':
                     self.label.configure(text="Downloaded!")
-                    open_folder_with_explorer()
+                    if self.openFolder:
+                        open_folder_with_explorer()
 
         def button_find_event():
             for filename in os.listdir("."):
@@ -78,14 +101,7 @@ class App(customtkinter.CTk):
                 }
                 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                     video_info = ydl.extract_info(entry, download=False)
-                    self.thumbnail_url = video_info.get("thumbnail")
-                    self.videotitle = video_info.get('title')
-                    self.duration = video_info.get("duration")
-                    self.author = video_info.get("uploader")
-                    self.format_id = video_info.get("format_id")  # format id of the video/audio
-                    self.ext = video_info.get("ext")  # file extension of the video/audio
-                    self.filesize = video_info.get("filesize")  # size of the video/audio file in bytes
-                    updateThumbnail(self.thumbnail_url)
+                    updateMediaInfo(entry)
                     ydl.download([entry])
 
             elif youtube in entry:
@@ -106,14 +122,7 @@ class App(customtkinter.CTk):
                     }
                 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                     video_info = ydl.extract_info(entry, download=False)
-                    self.thumbnail_url = video_info.get("thumbnail")
-                    self.videotitle = video_info.get('title')
-                    self.duration = video_info.get("duration")
-                    self.author = video_info.get("uploader")
-                    self.format_id = video_info.get("format_id")  # format id of the video/audio
-                    self.ext = video_info.get("ext")  # file extension of the video/audio
-                    self.filesize = video_info.get("filesize")  # size of the video/audio file in bytes
-                    updateThumbnail(self.thumbnail_url)
+                    updateMediaInfo(entry)
                     ydl.download([entry])
 
         def optionmenu_callback(choice):
@@ -126,6 +135,16 @@ class App(customtkinter.CTk):
                                                  command=optionmenu_callback)
         self.optionmenu.set("Select format")
         self.optionmenu.pack(padx=10, pady=10)
+
+        def switch_event():
+            if self.path_folder_switch.get() is "off":
+                self.openFolder = False
+            else:
+                self.openFolder = True
+        self.path_folder_switch = customtkinter.StringVar(value="on")
+        self.path_folder_switch = customtkinter.CTkSwitch(self, text="Open Folder after Download", command=switch_event,
+                                         variable=self.path_folder_switch, onvalue="on", offvalue="off")
+        self.path_folder_switch.pack()
 
         self.button_find = customtkinter.CTkButton(self, text="Download", command=button_find_event)
         self.button_find.pack(padx=10, pady=10)
@@ -172,9 +191,10 @@ class App(customtkinter.CTk):
                     except Exception as e:
                         print(e)
                     def returnThumbnailInfo():
-                        return f"{self.author}"+"\n"+f"{self.videotitle}"+"\n file format : "+f"{self.ext}"+"\n" + f"{self.filesize}"
+                        return f"{self.author}"+"\n"+f"{self.videotitle}"+"\n file format : "+f"{self.ext}"+"\n"
                     self.thumbnail_title = customtkinter.CTkLabel(self, text=returnThumbnailInfo())
-                    self.thumbnail_title.pack()
+                    if self.videotitle != "":
+                        self.thumbnail_title.pack()
 
 
 
